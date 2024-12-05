22aebc5839e6d86bc283027ee7539c5d85a9ba83
ghostofgamer <vadimcapital@mail.ru>
2024-12-05 13:55:39 +0400

    BildWebGLFlutterPrefWebViewPlugin

--- Assets/_Game/Scripts/Core/PlayerInput.cs
+++ Assets/_Game/Scripts/Core/PlayerInput.cs
@@ -14,22 +14,29 @@ public class PlayerInput : MonoBehaviour
 #if UNITY_ANDROID
         GetMobileInput();
 #endif
+
+#if UNITY_WEBGL
+        GetInput();
+#endif
     }
 
     private void GetInput()
     {
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
+            Debug.Log("GetUnput");
             _player.HandleJumpInput();
         }
     }
 
     private void GetMobileInput()
     {
+
         if (Input.touchCount > 0)
         {
+            Debug.Log("GetMobileUnput");
             Touch touch = Input.touches[0];
-            if (touch.phase == TouchPhase.Began && !EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+            if (touch.phase == TouchPhase.Began)
             {
                 _player.HandleJumpInput();
             }
